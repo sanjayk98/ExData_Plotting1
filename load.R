23e018ccb8dbbc107a2033b84f0e28dc5ed44cc5
sanjayk98 <33611968+sanjayk98@users.noreply.github.com>
2018-11-11 12:10:06 -0500

    Create load.R

---
+++ load.R
@@ -0,0 +1,7 @@
+# Author: Sanjay Kumar
+# Loading consumer power data from 1/2/2007 to 2/2/2007
+setwd("C:/Users/sanjayx/Desktop/coursera")
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",destfile = "c:/users/sanjayx/desktop/coursera/power.zip")
+consumption<-data.table::fread('household_power_consumption.txt')
+names(consumption)
+consume<-subset(consumption,consumption$Date=="1/2/2007"|consumption$Date=="2/2/2007")
