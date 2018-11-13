2cba7f60a9cd58eadb2a86955902b8172f7ac473
sanjayk98 <33611968+sanjayk98@users.noreply.github.com>
2018-11-12 20:24:13 -0500

    Add files via upload

--- plot2.R
+++ plot2.R
@@ -8,6 +8,6 @@ par(resetPar())
 png(filename = "plot2.png",width = 480, height = 480)
 dev.copy(png,'plot2.png')
 par(mfrow=c(1,1))
-plot(as.POSIXct(paste(power$Date, power$Time), format="%d/%m/%Y %H:%M:%S"),power$Global_active_power,type="l",col="red",main="global active power",xlab="Date",ylab="kw")
+plot(as.POSIXct(paste(power$Date, power$Time), format="%d/%m/%Y %H:%M:%S"),as.numeric(power$Global_active_power),type="l",col="red",main="global active power",xlab="Date",ylab="kw")
 
 dev.off()
